03227988eab622c6899ea3e9c3483cf7390cad64
lmatsuki <lmatsuki87@gmail.com>
2018-09-09 12:20:28 -0700

    Reverted server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Server.js, don't forget to add express & ejs to packages
-var app = require('express')(),
-    http_instance;
+const express = require('express')
 
+const app = express()
 const port = process.env.PORT || 80
 const router = express.Router()
 
@@ -17,25 +17,6 @@ router.get('/*', (req, res, next) => {
 
 app.use('/', router)
 
-app.get('/', function( req, res ){  
-    setTimeout( function(){ 
-      res.send( 'all done!' ); 
-    }, 10000 );
-  });
-
-// when shutdown signal is received, do graceful shutdown
-process.on( 'SIGINT', function(){
-    http_instance.close( function(){
-      console.log( 'gracefully shutting down :)' );
-      process.exit();
-    });
-  });
-
-var server = app.listen(port)
+app.listen(port)
 
 console.log('App running on port', port)
-
-// listen for an event
-var handler = function() {
-    server.close();
-  };
